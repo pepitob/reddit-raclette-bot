080204b2499c907567f6ed58efbf29a5345359d6
Hippolyte Bourban <hippolytebourban@gmail.com>
2023-02-12 06:57:39 +0100

    started project and fetch posts and comments

---
+++ bot.py
@@ -0,0 +1,33 @@
+import praw
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent="console:raclette-bot:1.0 (by u/Raclette_bot)",
+)
+
+subreddit = reddit.subreddit("france")
+
+for submission in subreddit.hot(limit=10):
+    print("****************")
+    print(submission.title)
+
+    for comment in submission.comments:
+        if hasattr(comment, "body"):
+            print("-------------------")
+            print(comment.body)
+
+
+def run_bot():
+    subreddit = reddit.subreddit("raclettetest")
+    for comment in subreddit.stream.comments(skip_existing=True):
+        if "raclette" in comment.body.lower() or "Raclette" in comment.body:
+            comment.reply("I love raclette too! It's such a delicious dish. 😋")
+
+while True:
+    run_bot()
+    time.sleep(30)
